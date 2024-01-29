dffc1ca8389299180fc4ec338e6962383603e555
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-29 17:33:14 +0100

    Aktualisierung der ListingListItem-Component

--- src/components/ListingsListItem.vue
+++ src/components/ListingsListItem.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="card h-100" :class="{ 'bg-dark': isDark, 'bg-light': !isDark }">
+  <div
+    class="card h-100"
+    :class="{ 'bg-dark': darkMode, 'bg-light': !darkMode }"
+  >
     <img :src="listing.image" class="card-img-top" />
     <div class="card-body">
       <h5
         class="card-title"
-        :class="{ 'text-white': isDark, 'text-black': !isDark }"
+        :class="{ 'text-white': darkMode, 'text-black': !darkMode }"
       >
         {{ listing.title }}
       </h5>
       <small
         class="card-text"
-        :class="{ 'text-white': isDark, 'text-black': !isDark }"
+        :class="{ 'text-white': darkMode, 'text-black': !darkMode }"
       >
         {{ listing.address }}
       </small>
       <p
         class="card-text mt-2"
-        :class="{ 'text-white': isDark, 'text-black': !isDark }"
+        :class="{ 'text-white': darkMode, 'text-black': !darkMode }"
       >
         {{ listing.description }}
 
@@ -24,7 +27,7 @@
       </p>
 
       <div class="my-3 text-center">
-        <small :class="{ 'text-white': isDark, 'text-black': !isDark }">
+        <small :class="{ 'text-white': darkMode, 'text-black': !darkMode }">
           <span>€{{ listing.price / 100 }}/Tag</span> ·
           <span>Bewertung: {{ listing.rating }}/5</span>
         </small>
@@ -33,8 +36,8 @@
         <button
           class="btn"
           :class="{
-            'btn-outline-light': isDark,
-            'btn-outline-danger': !isDark,
+            'btn-outline-light': darkMode,
+            'btn-outline-danger': !darkMode,
           }"
           @click="removeListing(listing)"
         >
@@ -47,14 +50,16 @@
 
 <script>
 import { useStore } from "vuex";
+import useDarkMode from "@/hooks/useDarkMode.js";
 // import { mapActions } from "vuex";
 // import useNotification from "../hooks/useNotification";
 
 export default {
   name: "ListingsListItem",
-  props: ["listing", "isDark"],
+  props: ["listing"],
   setup(props) {
     const store = useStore();
+    const { darkMode } = useDarkMode();
     // const { setNotification } = useNotification();
     const removeListing = () => {
       // setNotification("Element von der Liste entfernt.");
@@ -62,6 +67,7 @@ export default {
     };
     return {
       removeListing,
+      darkMode,
     };
   },
 };
